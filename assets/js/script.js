79492015d8cf38194b6d3047ac54dac88ff92348
pitcca <114925361+pitcca@users.noreply.github.com>
2022-11-02 11:14:52 +0300

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,104 @@
+// #1
+// var arr = ['Привет, ', 'мир', '!'];
+// alert(arr[0] + arr[1] + arr[2]);
+
+// #2
+// var arr = ['Привет, ', 'мир', '!'];
+// var str = arr[0] + arr[1] + arr[2];
+// alert(str);
+
+// #3
+// var arr = ['Привет, ', 'мир', '!'];
+// arr[0] = 'Пока, '; 
+// alert(arr);
+
+// #4
+// var obj = {'Коля':'1436', 'Вася':'878', 'Петя':'1750'};
+// alert(obj['Коля'])
+
+// #5
+// var arr = {
+// 	'ru':['голубой', 'красный', 'зеленый'],
+// 	'en':['blue', 'red', 'green'],
+// };
+// alert(arr['ru'][1]);
+
+// #1
+// var arr = ['a', 'b', 'c'];
+// alert(arr) 
+
+// #2
+// var arr = ['a', 'b', 'c'];
+// alert(arr[0])
+// alert(arr[1]) 
+// alert(arr[2])
+
+// #3
+// var arr = ['a', 'b', 'c', 'd'];
+// var str = arr[0] + '+' + arr[1] + ' , ' + arr[2] + '+' + arr[3];
+// alert(str);
+
+// #4
+// var arr = [2, 5, 3, 9];
+// var result = arr[0] * arr[1] + arr[2] * arr[3];
+// alert(result);
+
+// #1
+// var obj = {a: 1, b: 2, c: 3};
+// alert(obj['c']);
+// alert(obj.c);
+
+// #2
+// var obj = {Коля: '1000', Вася: '500', Петя: '200'};
+// alert(obj.Петя);
+// alert(obj.Коля);
+
+// #3
+// let week = {
+//     1: 'Понедельник',
+//     2: 'Вторник',
+//     3: 'Среда',
+//     4: 'Четрверг',
+//     5: 'Пятница',
+//     6: 'Суббота',
+//     7: 'Воскресенье',
+//     };
+//     alert(week[3]);
+
+// #4
+// var obj = {
+//     1: 'Понедельник',
+//     2: 'Вторник', 
+//     3: 'Среда',
+//     4: 'Четрверг',
+//     5: 'Пятница',
+//     6: 'Суббота',
+//     7: 'Воскресенье'
+//     };
+// var day = 3;
+// alert(obj[day]);
+
+// #1
+// var arr = [ [1, 2, 3], [4, 5, 6], [7,8,9] ]; 
+// alert(arr[1][0]);
+
+// #2
+// var obj = {js:['jQuery', 'Angular'], php: 'hello', css: 'world'};
+// alert(obj['js'][0]);
+
+// #3
+// var arr = {
+//     'ru':['Понедельник','Вторник', 'Среда', 'Четрверг', 'Пятница', 'Суббота', 'Воскресенье'],
+//     'en':['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Thursday', 'Saturday', 'Sunday'],
+//     };
+// alert('ru = '+arr['ru'][0]+'; en = '+arr['en'][2]);
+
+// #4
+// var lang = 'ru';
+// var day = 3;
+// var arr = {
+//     'ru':['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'],
+//     'en':['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
+//     };
+// alert(arr[lang][3]);
+
